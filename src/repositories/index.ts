98f347853d8b4b254efb5001906026f07c54ade0
SHUSHI <63992333+SHUSHI1137@users.noreply.github.com>
2023-11-02 10:56:08 +0700

    re-structure-errorHandlers

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -4,16 +4,16 @@ import { ICreateUserDto } from "../dto/user";
 export interface IUserExtended
   extends Pick<User, "id" | "name" | "username" | "registeredAt"> {}
 
-type CreationErrorType = "UNIQUE";
+// type CreationErrorType = "UNIQUE";
 
-export class UserCreateionError extends Error {
-  constructor(
-    public readonly type: CreationErrorType,
-    public readonly column: string
-  ) {
-    super();
-  }
-}
+// export class UserCreateionError extends Error {
+//   constructor(
+//     public readonly type: CreationErrorType,
+//     public readonly column: string
+//   ) {
+//     super();
+//   }
+// }
 
 export interface IUserRepository {
   create(user: ICreateUserDto): Promise<IUserExtended>;
