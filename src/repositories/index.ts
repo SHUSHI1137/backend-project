8cbeabb2dee83f8968f41a34dc16230a9eff9232
SHUSHI <63992333+SHUSHI1137@users.noreply.github.com>
2023-11-02 10:37:56 +0700

    handler error registeration

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -4,6 +4,17 @@ import { ICreateUserDto } from "../dto/user";
 export interface IUserExtended
   extends Pick<User, "id" | "name" | "username" | "registeredAt"> {}
 
+type CreationErrorType = "UNIQUE";
+
+export class UserCreateionError extends Error {
+  constructor(
+    public readonly type: CreationErrorType,
+    public readonly column: string
+  ) {
+    super();
+  }
+}
+
 export interface IUserRepository {
   create(user: ICreateUserDto): Promise<IUserExtended>;
 }
--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -1,22 +1,33 @@
-import { Prisma, PrismaClient, User } from "@prisma/client";
-import { IUserExtended, IUserRepository } from ".";
-import { ICreateUserDto, IUserDto } from "../dto/user";
+import { PrismaClient } from "@prisma/client";
+import { IUserExtended, IUserRepository, UserCreateionError } from ".";
+import { ICreateUserDto } from "../dto/user";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export default class UserRepository implements IUserRepository {
   private prisma: PrismaClient;
-
   constructor(prisma: PrismaClient) {
     this.prisma = prisma;
   }
+
   public async create(user: ICreateUserDto): Promise<IUserExtended> {
-    return await this.prisma.user.create({
-      data: user,
-      select: {
-        id: true,
-        name: true,
-        username: true,
-        registeredAt: true,
-      },
-    });
+    try {
+      return await this.prisma.user.create({
+        data: user,
+        select: {
+          id: true,
+          name: true,
+          username: true,
+          registeredAt: true,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === "P2002"
+      )
+        throw new UserCreateionError("UNIQUE", "username");
+
+      throw new Error(`${error}`);
+    }
   }
 }
